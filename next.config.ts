e0364f8a409523ac999ac73732572856e9beb9f6
TIMΛƧHΛN <kavishkatimashan@gmail.com>
2025-09-15 18:29:57 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  // basePath: "/boolean-landing", // replace with your repo name
-  // assetPrefix: "/boolean-landing/",
-  /* config options here */
-  // images: {
-  //   unoptimized: true, // required for `next export`
-  // },
+  basePath: isProd ? "/boolean-landing" : "",
+  assetPrefix: isProd ? "/boolean-landing/" : "",
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
