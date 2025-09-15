9a7b0393a2204b8411f735522b2be4796c525d74
TIMΛƧHΛN <kavishkatimashan@gmail.com>
2025-09-15 15:08:53 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
