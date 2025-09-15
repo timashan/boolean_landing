4075876dd193200a04a88565f36fe9092a93318c
TIMΛƧHΛN <kavishkatimashan@gmail.com>
2025-09-15 16:14:56 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/boolean-landing", // replace with your repo name
+  assetPrefix: "/boolean-landing/",
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
